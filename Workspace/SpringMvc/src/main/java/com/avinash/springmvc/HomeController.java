44c630f78f68d81c336fb42db4023a7a9fd71714
Avinash Gautam <avigautam1229@gmail.com>
2024-07-11 23:45:45 +0530

    Spring mvc

--- Workspace/SpringMvc/src/main/java/com/avinash/springmvc/HomeController.java
+++ Workspace/SpringMvc/src/main/java/com/avinash/springmvc/HomeController.java
@@ -1,8 +1,11 @@
 package com.avinash.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -13,17 +16,24 @@ public class HomeController
 	@GetMapping("/")
 	public String home()
 	{
-		return "index.jsp";
+		return "index";
 //		System.out.println("Home page request");
 	}
 	@RequestMapping("add")
-	public String add(HttpServletRequest req)
+	public String add(@RequestParam("num1")int i,@RequestParam("num2")int j, Model m /*HttpSession session*//*HttpServletRequest req*/)
 	{
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//		ModelAndView mv = new ModelAndView();
+		
 		int res = i+j;
-		HttpSession session = req.getSession();
-		session.setAttribute("num3", res);
-		return "result.jsp";
+//		
+//		mv.addObject("num3", res);
+		m.addAttribute("num3", res);
+//		mv.setViewName("result");
+//		HttpSession session = req.getSession();
+//		session.setAttribute("num3", res);
+//		return mv;
+		return "result";
 	}
 }
